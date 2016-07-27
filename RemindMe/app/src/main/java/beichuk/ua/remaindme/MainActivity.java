d47b5b9bbbedfff5958618b1a00a368ec91ad5c3
beychukVA <beychuk.v.a@gmail.com>
2016-07-27 22:08:13 +0300

    Implement Navigation Drawer.

--- RemindMe/app/src/main/java/beichuk/ua/remaindme/MainActivity.java
+++ RemindMe/app/src/main/java/beichuk/ua/remaindme/MainActivity.java
@@ -1,18 +1,19 @@
 package beichuk.ua.remaindme;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import beychuk.ua.remindme.R;
 
-public class MainActivity extends Activity
+public class MainActivity extends AppCompatActivity
 {
     private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initToolbar();
